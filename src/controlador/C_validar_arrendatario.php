ce42d8afb31d63a7622fcc5cbe77d5eba3861d73
Rasel <ruselcucho@gmail.com>
2024-07-17 11:58:50 -0500

    Commit 2

--- src/controlador/C_validar_arrendatario.php
+++ src/controlador/C_validar_arrendatario.php
@@ -13,17 +13,28 @@ if (isset($_GET['id'], $_GET['accion'])) {
         // echo $estado;
         $sql = $conexion->query("update `arrendatario` SET `arr_estado`='$estado' WHERE arr_id= $id");
         if ($sql == 1) { //si
-            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>Ok!</strong> Arrendatario ' . $accion . 'do correctamente
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>';
+        //     echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        // <strong>Ok!</strong> Arrendatario ' . $accion . 'do correctamente
+        // <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        // </div>';
+
+        echo '<script type="text/javascript">
+                alert("Arrendatario verificado correctamente");
+                window.location.href = "arrendatario.php";
+                </script>';
+
         // header("Location: " . $_SERVER['HTTP_REFERER']);
         } else { //si es 0, hay error
             // echo '<div class="alert alert-danger">Error al registrar persona</div>';
-            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>Alerta!</strong> Error al ' . $accion . ' arrendatario
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>';
+        //     echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        // <strong>Alerta!</strong> Error al ' . $accion . ' arrendatario
+        // <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        // </div>';
+
+        echo '<script type="text/javascript">
+                alert("Arrendatario rechazado correctamente");
+                window.location.href = "arrendatario.php";
+                </script>';
         }
     }
 }
